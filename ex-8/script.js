109f42a13a2c8b78c6ee6e32173da8820d0ed831
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-06-26 14:41:24 -0300

    add - (ex-8)

---
+++ ex-8/script.js
@@ -0,0 +1,46 @@
+//var a = false
+//var b = false
+
+//var c = a && b
+//console.log(c)
+
+
+
+
+//var idade = 25
+//var maior20 = idade >= 20
+//var menor30 = idade <= 30
+//var entre = maior20 && menor30
+//console.log("sua idade",idade)
+//console.log("maior que vinte anos :",maior20)
+//console.log("menor que trinta anos :",menor30)
+//console.log("esta entre 20 e 30 ? ",entre)
+
+
+
+//var idade = 9
+
+//var menor10 = idade <= 10
+//var maior60 = idade >= 60
+
+//var gratuidade = menor10 || maior60
+
+//console.log("sua idade :",idade,"anos")
+//console.log("menor que dez anos : ", menor10)
+//console.log("maior que dez anos : ", maior60)
+//console.log("tem direito a gratuidade ?? ",gratuidade)
+
+
+
+var idade = 15
+
+var maior18 = idade >= 18
+var menor50 = idade <= 50
+var pdcbb = maior18 && menor50
+
+console.log("sua idade: ", idade)
+console.log("maior que dezoito anos? ", maior18)
+console.log("menor que cinqueta anos? ", menor50)
+console.log("pode compra bebida ??", pdcbb)
+
+
