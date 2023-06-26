e03225fe11ec016b78fc3f171c0de8c3ee0a7a36
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-06-26 10:21:20 -0300

    add - (ex-4)

---
+++ ex-4/script.js
@@ -0,0 +1,18 @@
+//boolean 
+//null
+//underfined
+//number
+//string
+
+
+var a = 1
+var b = "1"
+
+var c = a + b
+
+console.log(a)
+console.log(b)
+console.log(1 + 1)
+
+console.log("ola" + " mundo")
+
