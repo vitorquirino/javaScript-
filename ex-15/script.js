57cfc4c4f8004a23775003ad95ae5d677332e068
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-06-27 17:32:03 -0300

    add - (ex-15)

---
+++ ex-15/script.js
@@ -0,0 +1,24 @@
+
+//var numero = 4
+
+//for(var i = 10; i <= numero; i++){
+
+//        console.log('executando o for pela '+ i + ' vez')
+//} 
+
+//console.log('acabou')
+
+
+
+
+ var numero = Math.random() *100
+ 
+ while (numero < 90) {
+    console.log(numero)
+     numero = Math.random() *100
+    
+}
+
+console.log(numero)
+console.log('ACABOU')
+
