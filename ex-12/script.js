7ab85bb4a27ed7bed32ff9b8a4193b1fa98d0976
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-06-27 12:49:21 -0300

    add - (ex-12)

---
+++ ex-12/script.js
@@ -0,0 +1,54 @@
+//var idade = 18
+
+//if  (idade < 18) {
+//    console.log('não pode comprar bebida!')
+//    console.log('volte futuramente')
+    
+//}
+//else{
+//    console.log('pode comprar bebida!')
+//    console.log('qual seu pedido?')
+    
+//}
+
+
+
+
+
+
+
+//var idade = 45
+
+//if (idade >= 35){
+  //  console.log('pode comprar a bebida!')
+  //  console.log('qual seu pedido?')
+//}
+
+//else if (idade >= 18){
+    //console.log('pode comprar a bebida!')
+    //console.log('mostre a indentidade?')
+//}
+
+//else{
+  //  console.log('não pode comprar bebida!')
+  //  console.log('volte futuramente')
+//}
+
+
+
+
+
+
+var idade = 77
+
+if (idade >= 18 && idade <= 70 ){
+    console.log('pode comprar!')
+    console.log('qual seu pedido!')
+
+}
+
+else {
+    console.log('não pode comprar')
+    console.log('volte fururamente!')
+}
+
