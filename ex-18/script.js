610b04722c17af00457d712a733a4a1188c06d2f
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-07-03 16:32:18 -0300

    add - (ex-18)

---
+++ ex-18/script.js
@@ -0,0 +1,24 @@
+//function media(n1,n2) {
+  //  var nota1 = n1
+  //  var nota2 = n2
+  //  var media = (nota1 + nota2) /2
+
+  //  return media;
+//}
+
+//var result1 = media(6,7)
+//var m = media
+
+//var resultado2 = m(2,3)
+
+
+//console.log(result1)
+//console.log(resultado2)
+
+
+
+function media(n1 , n2){
+    return (n1 + n2) /2
+}
+
+console.log(media(8,5))
