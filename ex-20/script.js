1564782a5b9d767496488c72ae4758f0363e75ff
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-07-04 14:17:21 -0300

    add - (ex-20)

---
+++ ex-20/script.js
@@ -0,0 +1,17 @@
+var aluno = { nome: 'igor',
+              notas: [7.5, 5.0]
+
+}
+
+
+var novaprop = 'last name'
+
+
+aluno.matricula = 12345;
+
+aluno[novaprop] = 'quirino'
+
+
+
+console.log(aluno)
+          
