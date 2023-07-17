e81ec6c0a16407305aa81dd43b6843bfe8af9155
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-07-17 11:17:10 -0300

    add - (ex-30)

---
+++ ex-30/script.js
@@ -0,0 +1,9 @@
+function imagemIN(elemento){
+    elemento.style.backgroundColor = 'red'
+    elemento.style.width = '400px'
+}
+
+function imagemUT(elemento){
+    elemento.style.backgroundColor = 'rgb(24, 102, 185)'
+    elemento.style.width = '90px'
+}
