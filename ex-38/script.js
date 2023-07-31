75d2089ee58987d385155cbf2544c6bdef19647b
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-07-31 12:00:29 -0300

    add- (ex-38)

---
+++ ex-38/script.js
@@ -0,0 +1,17 @@
+
+onload = function(){
+    let nome = this.localStorage.getItem("nome")
+    let h1 = document.getElementById("nome")
+    h1.innerHTML = nome
+}
+
+function atualizar(element){
+    let valor = element.value
+    console.log(valor)
+
+    let h1 = document.getElementById("nome")
+    h1.innerHTML = valor
+
+    localStorage.setItem("nome", valor)
+}
+
