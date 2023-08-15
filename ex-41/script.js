0955a9e362b7d26c9d65af407580673aef4377bd
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-08-15 12:18:25 -0300

    Update script.js

--- ex-41/script.js
+++ ex-41/script.js
@@ -1,12 +1,3 @@
-let ps = ["vitor","regina","jose","sandra","vitoria"]
-
-function sortear(){
-    let np = ps.length 
-
-    let ns = Math.floor(Math.random( ) * np)
-    document.getElementById('d').innerHTML = ps[ns]
-   
-}
 
 
 
