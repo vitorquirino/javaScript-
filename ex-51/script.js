62650654916a3d397b0c02b4293f3cd935b14dc2
vitorquirino <104791533+vitorquirino@users.noreply.github.com>
2023-09-18 11:47:02 -0300

    add

---
+++ ex-51/script.js
@@ -0,0 +1,18 @@
+function f(e){
+    console.log('teste')
+    console.log(e)
+}
+
+
+function f1(e){
+    console.log('teste f1')
+    console.log(e)
+    console.log(e.target)
+
+}
+
+window.onload = function() {
+    let h1 = document.getElementsByTagName('h1')[0]
+
+    h1.addEventListener("click",f1)
+}
